e35392df26e5877c0c9ceb575cb638bfae9798d0
Chinmay Revatgaonkaar <77978778+Cmann01@users.noreply.github.com>
2024-07-09 23:11:23 +0530

    Time: 749 ms (57.04%), Space: 57.4 MB (9.97%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        total_waiting_time = 0
+        current_time = 0
+        
+        for arrival, time in customers:
+            if current_time < arrival:
+                current_time = arrival
+            current_time += time
+            total_waiting_time += (current_time - arrival)
+        
+        return total_waiting_time / len(customers)
