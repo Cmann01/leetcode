98b00173e636b085eb54490e42335ab0ddc3d4c3
Chinmay Revatgaonkaar <77978778+Cmann01@users.noreply.github.com>
2024-08-10 17:54:30 +0530

    Time: 145 ms (43.08%), Space: 17.6 MB (38.74%) - LeetHub

---
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.py
@@ -0,0 +1,37 @@
+class Solution:
+    def regionsBySlashes(self, grid: List[str]) -> int:
+        n = len(grid)
+        # Create an expanded grid
+        expanded_grid = [[0] * (n * 3) for _ in range(n * 3)]
+        
+        # Fill the expanded grid
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == '/':
+                    expanded_grid[i * 3][j * 3 + 2] = 1
+                    expanded_grid[i * 3 + 1][j * 3 + 1] = 1
+                    expanded_grid[i * 3 + 2][j * 3] = 1
+                elif grid[i][j] == '\\':
+                    expanded_grid[i * 3][j * 3] = 1
+                    expanded_grid[i * 3 + 1][j * 3 + 1] = 1
+                    expanded_grid[i * 3 + 2][j * 3 + 2] = 1
+        
+        # Function to perform DFS
+        def dfs(x, y):
+            if x < 0 or y < 0 or x >= len(expanded_grid) or y >= len(expanded_grid) or expanded_grid[x][y] != 0:
+                return
+            expanded_grid[x][y] = 1
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
+        
+        # Count regions
+        regions = 0
+        for i in range(len(expanded_grid)):
+            for j in range(len(expanded_grid)):
+                if expanded_grid[i][j] == 0:
+                    dfs(i, j)
+                    regions += 1
+        
+        return regions
