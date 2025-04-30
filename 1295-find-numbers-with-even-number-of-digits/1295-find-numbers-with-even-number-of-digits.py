7d4a77529813ca2eaead1a2e4abf9cc9ff18db8f
Chinmay Revatgaonkaar <77978778+Cmann01@users.noreply.github.com>
2025-04-30 16:23:29 +0530

    Time: 0 ms (100%), Space: 17.8 MB (53.9%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
@@ -1,34 +1,7 @@
-class Solution(object):
-    def findNumbers(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        def count_digits(num):
-            """
-            Helper function to count the number of digits in a number.
-            """
-            count = 0
-            while num > 0:
-                num //= 10
-                count += 1
-            return count
-
-        # Initialize a variable to store the count of numbers with even digits
-        even_digit_count = 0
-
-        # Iterate through the given array
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count = 0
         for num in nums:
-            # Check if the number of digits is even
-            if count_digits(num) % 2 == 0:
-                even_digit_count += 1
-
-        return even_digit_count
-
-# Example usage
-solution = Solution()
-nums1 = [12, 345, 2, 6, 7896]
-print(solution.findNumbers(nums1))  # Output: 2
-
-nums2 = [555, 901, 482, 1771]
-print(solution.findNumbers(nums2))  # Output: 1
+            if len(str(num)) % 2 == 0:
+                count += 1
+        return count
